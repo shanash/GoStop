1a34b97ad9b224ecdc3629c5d3ba38a3928627f2
Seonghun <seonghun@g1play.co.kr>
2025-02-05 14:39:56 +0900

    인게임 플로우 관리 클래스 이름 변경

---
+++ Assets/Scripts/InGame.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class InGame : MonoBehaviour
+{
+    [SerializeField]
+    InputController inputController = null;
+
+    [SerializeField]
+    Transform localPlayer = null;
+
+    [SerializeField]
+    Transform leftPlayer = null;
+
+    [SerializeField]
+    Transform rightPlayer = null;
+
+    [SerializeField]
+    PlayAreaView playArea = null;
+
+    void Start()
+    {
+        _ = GameManager.Instance;
+        GameManager.I.InitPlayers(localPlayer, leftPlayer, rightPlayer);
+        GameManager.I.initPlayArea(playArea);
+        GameManager.I.SetDel(inputController);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnClickFlipFirst()
+    {
+        GameManager.I.Deck.FlipFirst();
+    }
+
+    public HwatuCard OnClickPop()
+    {
+        return GameManager.I.Deck.Pop();
+    }
+
+    public void OnClickShuffle()
+    {
+        GameManager.I.Deck.Shuffle();
+    }
+
+    public void OnClickDraw()
+    {
+        //GameManager.I.Draw();
+    }
+}
